6efab4817bc9f08f9907ed6fbf607dc9b78cd962
ArtemDid <artem.didenkojs@gmail.com>
2022-07-19 15:36:33 +0300

    add all sites

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,50 @@ let execTime = 0;
 
 let domen: any = "";
 
-const getCrawlerCallback = (url: string) => {
+let dataUrl: Array<string> = [
+  "https://www.themanual.com/food-and-drink/best-large-air-fryers/",
+  "https://www.digitaltrends.com/home/best-air-fryers/",
+  "https://www.21oak.com/shop/air-fryer-deals-prime-day/",
+  "https://www.cnn.com/cnn-underscored/reviews/best-air-fryer",
+  "https://www.dontwasteyourmoney.com/best-air-fryer/",
+  "https://www.forbes.com/sites/forbes-personal-shopper/2021/06/21/best-air-fryers-on-amazon/?sh=27540843d3a4",
+  "https://www.nbcnews.com/select/shopping/best-air-fryers-ncna1288553",
+  "https://nypost.com/article/best-air-fryers-on-amazon/",
+  "https://www.bustle.com/wellness/the-7-best-air-fryers-on-amazon-22794431",
+  "https://www.bobvila.com/articles/best-air-fryers/",
+  "https://www.wired.com/gallery/best-air-purifiers/",
+  "https://www.glamour.com/gallery/best-makeup-gift-sets",
+  "https://bestreviews.com/kitchen/air-fryers/best-air-fryers",
+  "https://www.purewow.com/home/best-air-fryers",
+  "https://www.reviewed.com/small-appliances/best-right-now/best-air-fryer-toaster-ovens",
+  "https://www.cnet.com/tech/computing/best-laptop/",
+  "https://www.dailymail.co.uk/best-buys/best-air-fryers-uk",
+  "https://nymag.com/strategist/strategist/fashion/",
+  "https://www.buzzfeed.com/hannahloewentheil/the-best-air-fryer-for-every-type-of-home-cook",
+  "https://www.lifesavvy.com/50046/the-best-air-fryers-for-health-cooking/",
+  "https://www.goodhousekeeping.com/appliances/a24630295/best-air-fryers-reviews/",
+  "https://www.popularmechanics.com/home/food-drink/a35180245/best-air-fryers/",
+  "https://www.usnews.com/360-reviews/home-goods/air-purifier",
+  "https://www.insider.com/guides/kitchen/best-air-fryer",
+  "https://www.gearhungry.com/best-dog-bark-collars/",
+];
+
+const getCrawlerCallback = (url: string, domen: string) => {
   return (err: any, res: any, done: any) => {
     if (err) throw err;
+
     let $ = res.$;
     try {
       const anchors = $("a");
-      // console.log("1");
       Object.keys(anchors).forEach(async (item) => {
         if (anchors[item].type === "tag") {
           const href: string = anchors[item].attribs.href
             ?.trim()
             .replace(/(.+)(\/)$/, "$1");
-          // console.log("2");
+
           const parsedUrl: string = isUrl(href) ? href : `${domen}${href}`;
 
           const start = Date.now();
-          // const isExists = urls.has(parsedUrl);
-          // const isExists = await client.get(parsedUrl);
-          // console.log("is: ", isExists);
           const isExists = await client.exists(
             //@ts-ignore
             parsedUrl,
@@ -44,36 +69,26 @@ const getCrawlerCallback = (url: string) => {
             }
           );
 
-          // console.log("is: ", isExists);
           const end = Date.now();
 
           execTime = end - start;
 
           try {
             if (parsedUrl && href.toString().includes(domen) && !isExists) {
-              // console.log("5");
-              // urls.add(href);
               i++;
-              // await client.set(href, 0);
-              // // console.log("***************8");
-              // href.startsWith(href)
-              //   ? crawlAllUrls(href)
-              //   : crawlAllUrls(`${url}${href}`);
 
-              // console.log("domen: ", domen);
+              console.log("domen: ", domen);
 
               await client.set(decodeURI(parsedUrl), 0);
 
-              crawlAllUrls(encodeURI(parsedUrl));
+              crawlAllUrls(encodeURI(parsedUrl), domen);
             }
           } catch (e) {
-            // console.log("mmmmmmmmmmmm");
             done();
           }
         }
       });
     } catch (e) {
-      // console.error(`Encountered an error crawling ${url}. Aborting crawl.`);
       console.log("bbbbbbbbb");
       done();
     }
@@ -81,62 +96,73 @@ const getCrawlerCallback = (url: string) => {
   };
 };
 
-function crawlAllUrls(url: string) {
+function crawlAllUrls(url: string, domen: string) {
   c.queue({
     uri: url,
-    callback: getCrawlerCallback(url),
+    callback: getCrawlerCallback(url, domen),
   });
 }
 
-let temp = "https://www.themanual.com/food-and-drink/best-large-air-fryers/";
+let arrOfUrl: Array<string>;
+let nameTable: string;
 
-let temp2 = temp.split(/\/\/|\//);
+for (const url_item of dataUrl) {
+  arrOfUrl = url_item.split(/\/\/|\//);
 
-domen = temp2[0] === "https:" ? temp2[0] + "//" + temp2[1] : null;
+  domen = arrOfUrl[0] === "https:" ? arrOfUrl[0] + "//" + arrOfUrl[1] : null;
 
-// console.log(domen.replace(/(.+)(\/)$/, "$1"));
+  nameTable =
+    arrOfUrl[1].split(".")[1] === "com"
+      ? arrOfUrl[1].split(".")[0]
+      : arrOfUrl[1].split(".")[1];
 
-crawlAllUrls("https://www.themanual.com");
+  // (async function temp() {
+  start(domen, nameTable);
+  // })();
+}
 
-setInterval(async () => {
-  console.log("-------- LOGS ---------");
-  console.log("urls size", i);
-  console.log("queue size", c.queueSize);
-  console.log("last execution time on .has", execTime);
-  // try {
-  const keys: [] = await client.sendCommand(["keys", "*"]);
-  // console.log(keys); // ["aXF","x9U","lOk",...]
-  let arrWithUrl = [];
-  let itemRedisValue: string | null = "";
-  for (const key of keys) {
-    // console.log("1", key);
-    itemRedisValue = await client.get(key);
-    // console.log("item: ", item);
-    if (itemRedisValue === "0") {
-      // console.log("2");
-      //@ts-ignore
-      arrWithUrl.push({ url: key });
-      // console.log("3");
-      //@ts-ignore
-      await client.getSet(key, "1");
-    }
+async function start(domen, nameTable) {
+  if (!(await db.schema.hasTable(nameTable))) {
+    await db.schema.createTable(nameTable, (table) => {
+      table.increments("id").primary().unsigned();
+      table.text("url").unique();
+    });
   }
 
-  await createUser(arrWithUrl);
+  crawlAllUrls(domen, domen);
 
-  // console.log("temp: ", arrWithUrl, arrWithUrl.length);
-  // } catch {
-  //   console.log("Error");
-  // }
-}, 5000);
+  setInterval(async () => {
+    console.log("-------- LOGS ---------");
+    console.log("urls size", i);
+    console.log("queue size", c.queueSize);
+    console.log("last execution time on .has", execTime);
+    console.log("domen ", domen);
 
-async function createUser(req: any) {
-  await db("customers")
+    const keys: [] = await client.sendCommand(["keys", "*"]);
+    let arrWithUrl = [];
+    let itemRedisValue: string | null = "";
+
+    for (const key of keys) {
+      itemRedisValue = await client.get(key);
+
+      if (itemRedisValue === "0") {
+        //@ts-ignore
+        arrWithUrl.push({ url: key });
+        await client.getSet(key, "1");
+      }
+    }
+
+    await createUrl(arrWithUrl, nameTable);
+  }, 5000);
+}
+
+async function createUrl(req: any, nameTable: string) {
+  await db(nameTable)
     .insert(req)
     .onConflict("url")
     .ignore()
     .then(() => {
-      console.log("Ok");
+      console.log("Ok ", nameTable);
     })
     .catch((error: any) => {
       console.log("error: ,", error);
